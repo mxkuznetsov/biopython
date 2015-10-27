70c59c86eeec6835dc73fca9a92985dc1fc0bd0a
Maria Kuznetsov <mxk727@case.edu>
2015-10-27 11:26:50 -0400

    Will print full id of waters

--- test1.py
+++ test1.py
@@ -15,10 +15,14 @@ def atom_choosing():
 		                full_id = residue.get_full_id()
 		                if  "W" in full_id[3][0]:
 		                	print full_id
-		                #	print full_id[3][0]
+		              
             		except:
                			pass
-	return
+	
+print """This program will return the full ids of every water in this structure.
+		Read as follows: structure, model, chain, residue id.
+		The residue id reads (water, position of residue, insertion code)."""
+
 
 parser = PDBParser(PERMISSIVE=1)
 filename = raw_input('What is the file name? The format should be xxx.pdb\n')
